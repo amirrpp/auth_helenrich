d78e0e2daf95e56a28529ce243cceb777cb2c195
pluton <amirrpp@yandex.ru>
2016-02-07 09:09:16 +0200

    added login/registration view

--- webaccount/views.py
+++ webaccount/views.py
@@ -1,3 +1,119 @@
-from django.shortcuts import render
+from django import http
+from django.conf import settings
+from django.contrib.auth import login, authenticate, get_user_model
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .forms import EmailAuthenticationForm, EmailUserCreationForm
+
+User = get_user_model()
+
+
+class AccountAuthView(TemplateView):
+    template_name = 'auth/login_registration.html'
+    login_prefix, registration_prefix = 'login', 'registration'
+    login_form_class = EmailAuthenticationForm
+    registration_form_class = EmailUserCreationForm
+    redirect_field_name = 'next'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        return super(AccountAuthView, self).get(
+            request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(AccountAuthView, self).get_context_data(**kwargs)
+        if 'login_form' not in kwargs:
+            ctx['login_form'] = self.get_login_form()
+        if 'registration_form' not in kwargs:
+            ctx['registration_form'] = self.get_registration_form()
+        return ctx
+
+    def post(self, request, *args, **kwargs):
+        # Use the name of the submit button to determine which form to validate
+        if u'login_submit' in request.POST:
+            return self.validate_login_form()
+        elif u'registration_submit' in request.POST:
+            return self.validate_registration_form()
+        return http.HttpResponseBadRequest()
+
+    def get_login_form(self, bind_data=False):
+        return self.login_form_class(
+            **self.get_login_form_kwargs(bind_data))
+
+    def get_login_form_kwargs(self, bind_data=False):
+        kwargs = {}
+        kwargs['host'] = self.request.get_host()
+        kwargs['prefix'] = self.login_prefix
+        kwargs['initial'] = {
+            'redirect_url': self.request.GET.get(self.redirect_field_name, ''),
+        }
+        if bind_data and self.request.method in ('POST', 'PUT'):
+            kwargs.update({
+                'data': self.request.POST,
+                'files': self.request.FILES,
+            })
+        return kwargs
+
+    def validate_login_form(self):
+        form = self.get_login_form(bind_data=True)
+        if form.is_valid():
+
+            login(self.request, form.get_user())
+
+            return redirect(self.get_login_success_url(form))
+
+        ctx = self.get_context_data(login_form=form)
+        return self.render_to_response(ctx)
+
+    def get_login_success_url(self, form):
+        redirect_url = form.cleaned_data['redirect_url']
+        if redirect_url:
+            return redirect_url
+
+        return settings.LOGIN_REDIRECT_URL
+
+    def get_registration_form(self, bind_data=False):
+        return self.registration_form_class(
+            **self.get_registration_form_kwargs(bind_data))
+
+    def get_registration_form_kwargs(self, bind_data=False):
+        kwargs = self.get_login_form_kwargs(bind_data=bind_data)
+        kwargs['prefix'] = self.registration_prefix
+
+        return kwargs
+
+    def validate_registration_form(self):
+        form = self.get_registration_form(bind_data=True)
+        if form.is_valid():
+            self.register_user(form)
+
+            return redirect(self.get_registration_success_url(form))
+
+        ctx = self.get_context_data(registration_form=form)
+        return self.render_to_response(ctx)
+
+    def get_registration_success_url(self, form):
+        redirect_url = form.cleaned_data['redirect_url']
+        if redirect_url:
+            return redirect_url
+
+        return settings.LOGIN_REDIRECT_URL
+
+    def register_user(self, form):
+        user = form.save()
+        try:
+            user = authenticate(
+                username=user.username,
+                password=form.cleaned_data['password1'])
+        except User.MultipleObjectsReturned:
+            users = User.objects.filter(email=user.email)
+            user = users[0]
+            for u in users[1:]:
+                u.is_active = False
+                u.save()
+
+        login(self.request, user)
+
+        return user
